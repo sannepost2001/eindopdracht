bfa3914689b449fe39a5b8cd8ecc1a08da421cca
sannepost2001 <43266412+sannepost2001@users.noreply.github.com>
2019-05-08 11:57:19 +0200

    test3

--- app.py
+++ app.py
@@ -21,34 +21,6 @@ def hello_world():
             '</body>\n'
             '</html>')
 
-@app.route('/', methods=['POST'])
-def zoeken():
-    conn = mysql.connector.connect(host="", user="", db="")
-    cursor = conn.cursor()
-    zoekwoord = request.form["zoekwoord"]
-    cursor.execute("select * from  where  like '%" + zoekwoord + "%'")
-    records = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    teruggeven = ""
-    for row in records:
-        teruggeven = teruggeven+(row[0])
-        teruggeven = teruggeven + "<br>"
-
-    return ('<!DOCTYPE html>\n'
-            '<html lang="en">\n'
-            '<head>\n'
-            '    <meta charset="UTF-8">\n'
-            '    <title>Afvink2</title>\n'
-            '</head>\n'
-            '<body>\n'
-            '<form method = "POST">Zoekwoord:\n'
-            '    <input type="text" name = "zoekwoord">\n'
-            '    <input type ="submit" value="Submit"></form>\n'
-            + "<br>" + 'Het volgende is gevonden met je gegeven zoekwoord '
-            + zoekwoord + " : <br>" + "<br>" + teruggeven +
-            '</body>\n'
-            '</html>')
 
 if __name__ == '__main__':
     app.run()
